ea04047e06c9d3cd4af407214e5a753ce63012aa
Douglas Andrade <andrade.douglasdias@gmail.com>
2022-08-28 18:19:50 -0300

    Criado metodos CREATE e READ na rota restarantes

--- src/routes/restaurantes.js
+++ src/routes/restaurantes.js
@@ -1,8 +1,47 @@
-const express = require('express')
-const route = express.Router
+const pool = require('../helpers/database')
 
-route.get('/', (req, res) => {
-  res.status(200).json('Rota Restaurantes')
+const express = require('express')
+const router = express.Router()
+
+// Rota que retorna uma lista de todos os restaurantes cadastrados no DB
+router.get('/', async (req, res) => {
+  try {
+    const readRestaurantesQuery = 'SELECT * FROM restaurantes;'
+    const rows = await pool.query(readRestaurantesQuery)
+
+    res.status(200).json(rows)
+  } catch (error) {
+    res.status(400).json(error)
+  }
 })
 
-module.exports = route
+// Rota que retorna um restaurante pelo Id informado na URL
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const readRestauranteByIdQuery = 'SELECT * FROM restaurantes WHERE id = ?;'
+    const rows = await pool.query(readRestauranteByIdQuery, id)
+
+    res.status(200).json(rows)
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+// Rota que gera um novo registro de restaurantes no DB
+router.post('/', async (req, res) => {
+  const { img_path, name, address } = req.body
+
+  try {
+    const insertRestauranteQuery =
+      'INSERT INTO restaurantes (img_path, name, address) VALUES (?, ?, ?)'
+    await pool.query(insertRestauranteQuery, [img_path, name, address])
+
+    res.status(200).json('message: Novo registro inserido com sucesso!')
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+module.exports = router
